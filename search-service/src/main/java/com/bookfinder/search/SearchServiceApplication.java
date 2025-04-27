e85cec934170de50317203a05ea88e416a967dd3
Sturon <sturongame@gmail.com>
2025-04-27 11:18:43 +0500

    ФИНАЛОЧКА

--- search-service/src/main/java/com/bookfinder/search/SearchServiceApplication.java
+++ search-service/src/main/java/com/bookfinder/search/SearchServiceApplication.java
@@ -7,15 +7,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.bookfinder.search.client")
 @SpringBootApplication(
         exclude = {
                 DataSourceAutoConfiguration.class,
                 HibernateJpaAutoConfiguration.class,
                 FlywayAutoConfiguration.class
-        })
-@EnableFeignClients
+        }
+)
 public class SearchServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SearchServiceApplication.class, args);
     }
 }
+
--- search-service/src/main/java/com/bookfinder/search/client/controller/SearchController.java
+++ search-service/src/main/java/com/bookfinder/search/client/controller/SearchController.java
@@ -6,6 +6,8 @@ import com.bookfinder.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.bookfinder.search.client.CatalogClient;
+
 
 import java.util.List;
 
@@ -15,12 +17,18 @@ import java.util.List;
 public class SearchController {
 
     private final SearchService service;
+    private final CatalogClient catalogClient;   // <-- добавили сюда
 
     @PostMapping
     public ResponseEntity<List<BookDto>> search(@RequestBody SearchRequestDto req) {
         List<BookDto> books = service.search(req.getAuthor(), req.getTitle())
                 .collectList()
                 .block();
+
+        if (books != null && !books.isEmpty()) {
+            catalogClient.saveBatch(books);  // <-- теперь этот код компилируется
+        }
+
         return ResponseEntity.ok(books);
     }
 }
